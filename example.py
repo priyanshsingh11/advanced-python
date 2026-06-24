ec0a3042e04fdc0eb8cfbf763511931c633f0090
Priyansh Singh <priyanshsingh855@gmail.com>
2026-06-24 12:17:51 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,29 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def greet(name: str) -> str:
+    return f"Hello {name}"
+
+
+def square(x: int) -> int:
+    return x * x
+
+
+def area(length: float, width: float) -> float:
+    return length * width
+
+
+# Exercise 1
+def volume(length, width, height):
+    return length * width * height
+
+
+# Exercise 2
+def is_even(number):
+    return number % 2 == 0
+
+
+# Exercise 3
+def full_name(first, last):
+    return first + " " + last
